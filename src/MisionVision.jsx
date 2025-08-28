199ed056ccf7ae785e03900215b695a3e2f1988c
AmirOrozco11 <amorozcoes@ittepic.edu.mx>
2025-08-28 11:26:34 -0700

    mision y vison version 1.1

---
+++ src/MisionVision.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const MisionVision = () => (
+  <section className="mision-vision-section">
+    <div className="section-container">
+      <h3>Misión</h3>
+      <p>Nuestra misión es brindar soluciones de calidad y confianza a nuestros clientes, contribuyendo al desarrollo y bienestar de la comunidad.</p>
+      <h3>Visión</h3>
+      <p>Ser reconocidos como líderes en nuestro sector, innovando y creciendo junto a nuestros clientes y colaboradores.</p>
+    </div>
+  </section>
+);
+
+export default MisionVision;
